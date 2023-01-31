5ff39b7728bb221fa947a04c854e0fa3e7e34e61
marcelrm11 <marcelrm11@gmail.com>
2023-01-31 14:02:29 +0100

    changed tablename to users to avoid conflict with reserved keyword user and tested connection to elephandsql db

--- src/api/models.py
+++ src/api/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
