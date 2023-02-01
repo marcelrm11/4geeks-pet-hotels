b147a5691eb28c8716205d55e8bf48c5dc50d63b
Victoria <victoria1998castellar@hotmail.com>
2023-02-01 10:13:31 +0000

    signup view complete

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -26,7 +29,7 @@ def get_users():
 
     return jsonify(response_body), 200
 
-@api.route('/user', methods=['POST'])
+@api.route('/signup', methods=['POST'])
 def create_user():
     try:
         email = request.json.get("email", None)
--- src/app.py
+++ src/app.py
@@ -11,6 +11,7 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager
 
 #from models import Person
 
@@ -18,6 +19,10 @@ ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "fgpwoenvpse4935udv034"  # Change this!
+jwt = JWTManager(app)
 
 # database configuration
 db_url = os.getenv("DATABASE_URL")
