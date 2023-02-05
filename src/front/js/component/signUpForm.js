099fec3de4d58cac6673458fcad37591d8a0fffd
marcelrm11 <marcelrm11@gmail.com>
2023-02-05 14:58:52 +0100

    creates a map list in the signup form

--- src/front/js/component/signUpForm.js
+++ src/front/js/component/signUpForm.js
@@ -5,6 +5,14 @@ import { Input } from "./input.js";
 export const SignUpForm = ({ formData, handleChange, handleValidate }) => {
   const { store } = useContext(Context);
 
+  const capitalize = (word) => {
+    const wordArr = word.split("");
+    return wordArr[0].toUpperCase() + wordArr.slice(1).join("");
+  };
+  const removeUnderscores = (word) => {
+    return word.replaceAll("_", " ");
+  };
+
   return (
     <form>
       <input
@@ -13,70 +21,20 @@ export const SignUpForm = ({ formData, handleChange, handleValidate }) => {
         value={formData.crsf_token}
         onChange={handleChange}
       />
-      <Input
-        type="text"
-        placeholder="First name"
-        id="first_name"
-        value={formData.first_name}
-        onChange={handleChange}
-      />
-      {store.errors.first_name && <p>{store.errors.first_name}</p>}
-      <Input
-        type="text"
-        placeholder="Last name"
-        id="last_name"
-        value={formData.last_name}
-        onChange={handleChange}
-      />
-      {store.errors.last_name && <p>{store.errors.last_name}</p>}
-      <Input
-        type="text"
-        placeholder="Email"
-        id="email"
-        value={formData.email}
-        onChange={handleChange}
-      />
-      {store.errors.email && <p>{store.errors.email}</p>}
-      <Input
-        type="password"
-        placeholder="Password"
-        id="password"
-        value={formData.password}
-        onChange={handleChange}
-      />
-      {store.errors.password && <p>{store.errors.password}</p>}
-      <Input
-        type="password"
-        placeholder="Confirm password"
-        id="confirm_password"
-        value={formData.confirm_password}
-        onChange={handleChange}
-      />
-      {store.errors.confirm_password && <p>{store.errors.confirm_password}</p>}
-      <Input
-        type="text"
-        placeholder="Country"
-        id="country"
-        value={formData.country}
-        onChange={handleChange}
-      />
-      {store.errors.country && <p>{store.errors.country}</p>}
-      <Input
-        type="text"
-        placeholder="Zip code"
-        id="zip_code"
-        value={formData.zip_code}
-        onChange={handleChange}
-      />
-      {store.errors.zip_code && <p>{store.errors.zip_code}</p>}
-      <Input
-        type="text"
-        placeholder="Phone number"
-        id="phone_number"
-        value={formData.phone_number}
-        onChange={handleChange}
-      />
-      {store.errors.phone_number && <p>{store.errors.phone_number}</p>}
+      {Object.entries(formData).map(([field, value]) => {
+        return (
+          <React.Fragment key={field}>
+            <Input
+              type={field.includes("password") ? "password" : "text"}
+              id={field}
+              placeholder={removeUnderscores(capitalize(field))}
+              value={formData[field]}
+              onChange={handleChange}
+            />
+            {store.errors[field] && <p>{store.errors[field]}</p>}
+          </React.Fragment>
+        );
+      })}
       <button onClick={handleValidate}>Sign up</button>
     </form>
   );
