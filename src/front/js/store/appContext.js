7fe688f1d547dbc24bacc3d81fbbd94888f1d091
Victoria <victoria1998castellar@hotmail.com>
2023-02-05 10:05:25 +0000

    cleaning unnecessary components from boilerplate

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -29,7 +29,7 @@ const injectContext = (PassedComponent) => {
        * store, instead use actions, like this:
        **/
 
-      state.actions.tokensessionStore();
+      state.actions.tokenSessionStore();
     }, []);
 
     // The initial value for the context is not null anymore, but the current state of this component,
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,30 +2,30 @@ const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
       token: null,
-      message: null,
-      demo: [
-        {
-          title: "FIRST",
-          background: "white",
-          initial: "white",
-        },
-        {
-          title: "SECOND",
-          background: "white",
-          initial: "white",
-        },
-      ],
+      // message: null,
+      // demo: [
+      //   {
+      //     title: "FIRST",
+      //     background: "white",
+      //     initial: "white",
+      //   },
+      //   {
+      //     title: "SECOND",
+      //     background: "white",
+      //     initial: "white",
+      //   },
+      // ],
     },
     actions: {
       login: async (email, password) => {
         const opt = {
           method: "POST",
           headers: {
-            "content-type": "application/json",
+            "Content-Type": "application/json"
           },
           body: JSON.stringify({
-            email: "test",
-            password: "test",
+            email: email,
+            password: password,
           }),
         };
 
@@ -49,56 +49,56 @@ const getState = ({ getStore, getActions, setStore }) => {
         }
       },
 
-      tokensessionStore: () => {
+      tokenSessionStore: () => {
         const token = sessionStorage.getItem("token");
         if (token && token != "" && token != undefined)
           setStore({ token: token });
       },
       // Use getActions to call a function within a fuction
-      exampleFunction: () => {
-        getActions().changeColor(0, "green");
-      },
+      // exampleFunction: () => {
+      //   getActions().changeColor(0, "green");
+      // },
 
       logout: () => {
         sessionStorage.removeItem("token");
         setStore({ token: null });
       },
 
-      getMessage: async () => {
-        const store = getStore();
-        const opt = {
-          headers: { Authorization: "Bearer " + store.token },
-        };
+      // getMessage: async () => {
+      //   const store = getStore();
+      //   const opt = {
+      //     headers: { Authorization: "Bearer " + store.token },
+      //   };
 
-        try {
-          // fetching data from the backend
-          const resp = await fetch(
-            "https://3001-marcelrm11-4geekspethot-khhwppgad2k.ws-eu84.gitpod.io/api/hello",
-            opt
-          );
-          const data = await resp.json();
-          setStore({ message: data.message });
-          console.log("funciono", data);
-          // don't forget to return something, that is how the async resolves
-          return data;
-        } catch (error) {
-          console.error("Error loading message from backend", error);
-        }
-      },
-      changeColor: (index, color) => {
-        //get the store
-        const store = getStore();
+      //   try {
+      //     // fetching data from the backend
+      //     const resp = await fetch(
+      //       "https://3001-marcelrm11-4geekspethot-khhwppgad2k.ws-eu84.gitpod.io/api/hello",
+      //       opt
+      //     );
+      //     const data = await resp.json();
+      //     setStore({ message: data.message });
+      //     console.log("funciono", data);
+      //     // don't forget to return something, that is how the async resolves
+      //     return data;
+      //   } catch (error) {
+      //     console.error("Error loading message from backend", error);
+      //   }
+      // },
+      // changeColor: (index, color) => {
+      //   //get the store
+      //   const store = getStore();
 
-        //we have to loop the entire demo array to look for the respective index
-        //and change its color
-        const demo = store.demo.map((elm, i) => {
-          if (i === index) elm.background = color;
-          return elm;
-        });
+      //   //we have to loop the entire demo array to look for the respective index
+      //   //and change its color
+      //   const demo = store.demo.map((elm, i) => {
+      //     if (i === index) elm.background = color;
+      //     return elm;
+      //   });
 
-        //reset the global store
-        setStore({ demo: demo });
-      },
+      //   //reset the global store
+      //   setStore({ demo: demo });
+      // },
     },
   };
 };
