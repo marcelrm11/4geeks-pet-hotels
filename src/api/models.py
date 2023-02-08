73f5b42c0d845a2bc2c6b7adf9b216cd4d24001f
Victoria <victoria1998castellar@hotmail.com>
2023-02-08 20:29:13 +0000

    added owner and hotel tables

--- src/api/models.py
+++ src/api/models.py
@@ -14,8 +14,6 @@ class User(db.Model):
     country = db.Column(db.String(30), nullable=False)
     zip_code = db.Column(db.String(30), nullable=False)
     phone_number = db.Column(db.String(), nullable=False)
-    pets = db.relationship('Pets', backref=db.backref("users"))
-
     def __repr__(self):
         return f'<User {self.email}>'
 
@@ -52,6 +50,8 @@ class Pets(db.Model):
     age = db.Column(db.Integer, nullable=False)
     health = db.Column(db.String(50), nullable=False)
     pet_owner = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user_pet = db.relationship('User', backref=db.backref("users"))
+
 
     def __repr__(self):
         return f'<Pets {self.name}>'
@@ -66,20 +66,56 @@ class Pets(db.Model):
             "Health": self.health
             # do not serialize the password, its a security breach
         }
+    
+class Hotel(db.Model):
+    __tablename__ = 'hotel'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    country = db.Column(db.String(30), nullable=False)
+    zip_code = db.Column(db.String(30), nullable=False)
+    phone_number = db.Column(db.String(), nullable=False)
+    location = db.Column(db.String(70), nullable=False)
+    services = db.Column(db.String(100), nullable=False)
+    hotel_owner = db.Column(db.Integer, db.ForeignKey("owner.id"), nullable=False)
+    owner = db.relationship("Owner", backref=db.backref("owners"))
+
+   
+
+    def __repr__(self):
+        return f'<Hotel {self.name}>'
+
+    # # NOTE: In a real application make sure to properly hash and salt passwords
+    # def check_password(self, password):
+    #     return check_password(password, "password")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "location": self.location,
+            "phone_number": self.phone_number,
+            "country": self.country,
+            "zip_code": self.zip_code
+            # do not serialize the password, its a security breach
+        }
         
 class Booking(db.Model):
     __tablename__ = 'booking'
     id = db.Column(db.Integer, primary_key=True) 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    user_name = db.Column(db.String(50), db.ForeignKey("user.first_name"), nullable=False)
-    user_email = db.Column(db.String(50), db.ForeignKey("user.email"), nullable=False)
+    # user_name = db.Column(db.String(50), db.ForeignKey("user.first_name"), nullable=False)
+    # user_email = db.Column(db.String(50), db.ForeignKey("user.email"), nullable=False)
     hotel_id = db.Column(db.Integer, db.ForeignKey("hotel.id"), nullable=False)
     create_date = db.Column(db.Integer, nullable=True)
     stay_date = db.Column(db.Integer, nullable=True)
-    hotel_email = db.Column(db.String(50), db.ForeignKey("hotel.email"),nullable=False)
-    hotel_name = db.Column(db.String(50), db.ForeignKey("hotel.name"),nullable=False)
+    # hotel_email = db.Column(db.String(50), db.ForeignKey("hotel.email"),nullable=False)
+    # hotel_name = db.Column(db.String(50), db.ForeignKey("hotel.name"),nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    user = db.relationship("User", backref=db.backref("users", lazy='dynamic'), foreign_keys=[user_id])
+    user = db.relationship("User", backref=db.backref("bookings", lazy='dynamic'))
+    hotel = db.relationship("Hotel", backref=db.backref("bookings", lazy='dynamic'))
+
 
     def __repr__(self):
         return f'<Booking {self.id}>'
@@ -99,3 +135,34 @@ class Booking(db.Model):
             # do not serialize the password, its a security breach
         }
 
+
+class Owner(db.Model):
+    __tablename__ = 'owner'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(32), unique=False, nullable=False)
+    confirm_password = db.Column(db.String(32), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True) #? what is this doing here?
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    country = db.Column(db.String(30), nullable=False)
+    zip_code = db.Column(db.String(30), nullable=False)
+    phone_number = db.Column(db.String(), nullable=False)
+    
+    def __repr__(self):
+        return f'<Hotel_owner {self.email}>'
+
+    # # NOTE: In a real application make sure to properly hash and salt passwords
+    # def check_password(self, password):
+    #     return check_password(password, "password")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "country": self.country,
+            "zip_code": self.zip_code
+            # do not serialize the password, its a security breach
+        }
