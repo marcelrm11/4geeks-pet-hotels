792e3b6649b0ac46f8402d1ee71253217605dcf5
marcelrm11 <marcelrm11@gmail.com>
2023-02-10 08:38:37 +0100

    formats models.py

--- src/api/models.py
+++ src/api/models.py
@@ -1,30 +1,30 @@
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(32), unique=False, nullable=False)
     confirm_password = db.Column(db.String(32), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=False) #? what is this doing here?
+    # ? what is this doing here?
+    is_active = db.Column(db.Boolean(), unique=False,
+                          nullable=False, default=False)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     country = db.Column(db.String(30), nullable=False)
     zip_code = db.Column(db.String(30), nullable=False)
     phone_number = db.Column(db.String(), nullable=False)
-    user_pets = db.relationship('Pets', backref=db.backref("user"))
+    user_pets = db.relationship("Pets", backref=db.backref("user"))
     user_bookings = db.relationship("Booking", backref=db.backref("user"))
     invoices = db.relationship("Invoice", backref=db.backref("user"))
     favorites = db.relationship("Favorite", backref=db.backref("user"))
 
     def __repr__(self):
-        return f'<User {self.email}>'
-
-    # # NOTE: In a real application make sure to properly hash and salt passwords
-    # def check_password(self, password):
-    #     return check_password(password, "password")
+        return f"<User {self.email}>"
 
     def serialize(self):
         return {
@@ -35,24 +35,27 @@ class User(db.Model):
             "country": self.country,
             "zip_code": self.zip_code,
             "user_pets": [pet.serialize() for pet in self.user_pets],
-            "user_bookings": [booking.serialize() for booking in self.user_bookings], # could we remove this?
-            "favorites": [fav.serialize() for fav in self.favorites] # could we remove this?
-            # do not serialize the password, its a security breach
+            # could we remove this?
+            "user_bookings": [booking.serialize() for booking in self.user_bookings],
+            # could we remove this?
+            "favorites": [fav.serialize() for fav in self.favorites]
         }
 
+
 class Pets(db.Model):
-    __tablename__ = 'pets'
+    __tablename__ = "pets"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     pet_type = db.Column(db.String(50), nullable=False)
     breed = db.Column(db.String(50), nullable=False, default="N/A")
     age = db.Column(db.Integer, nullable=False)
     health = db.Column(db.String(50), nullable=False)
-    pet_owner_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    pet_owner_id = db.Column(
+        db.Integer, db.ForeignKey("user.id"), nullable=False)
     bookings = db.relationship("Booking", backref=db.backref("pets"))
 
     def __repr__(self):
-        return f'<Pet {self.name}>'
+        return f"<Pet {self.name}>"
 
     def serialize(self):
         return {
@@ -65,6 +68,7 @@ class Pets(db.Model):
             "pet_owner": self.user.serialize()
         }
 
+
 class Favorite(db.Model):
     __tablename__ = "favorite"
     id = db.Column(db.Integer, primary_key=True)
@@ -72,7 +76,7 @@ class Favorite(db.Model):
     hotel_id = db.Column(db.Integer, db.ForeignKey("hotel.id"), nullable=False)
 
     def __repr__(self):
-        return f'<Favorite_id {self.id}>'
+        return f"<Favorite_id {self.id}>"
 
     def serialize(self):
         return {
@@ -82,13 +86,16 @@ class Favorite(db.Model):
             "hotel_id": self.hotel_id
         }
 
+
 class Owner(db.Model):
-    __tablename__ = 'owner'
+    __tablename__ = "owner"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(32), unique=False, nullable=False)
     confirm_password = db.Column(db.String(32), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=False) #? what is this doing here?
+    # ? what is this doing here?
+    is_active = db.Column(db.Boolean(), unique=False,
+                          nullable=False, default=False)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     country = db.Column(db.String(30), nullable=False)
@@ -97,7 +104,7 @@ class Owner(db.Model):
     hotels = db.relationship("Hotel", backref=db.backref("owner"))
 
     def __repr__(self):
-        return f'<Hotel_owner {self.email}>'
+        return f"<Hotel_owner {self.email}>"
 
     def serialize(self):
         return {
@@ -109,9 +116,10 @@ class Owner(db.Model):
             "zip_code": self.zip_code,
             "hotels": [h.serialize() for h in self.hotels]
         }
-    
+
+
 class Hotel(db.Model):
-    __tablename__ = 'hotel'
+    __tablename__ = "hotel"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), nullable=False)
     name = db.Column(db.String(50), nullable=False)
@@ -120,14 +128,15 @@ class Hotel(db.Model):
     phone_number = db.Column(db.String(), nullable=False)
     location = db.Column(db.String(70), nullable=False)
     services = db.Column(db.String(100), nullable=False)
-    hotel_owner_id = db.Column(db.Integer, db.ForeignKey("owner.id"), nullable=False)
+    hotel_owner_id = db.Column(
+        db.Integer, db.ForeignKey("owner.id"), nullable=False)
     hotel_bookings = db.relationship("Booking", backref=db.backref("hotel"))
     invoices = db.relationship("Invoice", backref=db.backref("hotel"))
     favorites = db.relationship("Favorite", backref=db.backref("hotel"))
     rooms = db.relationship("Room", backref=db.backref("hotel"))
 
     def __repr__(self):
-        return f'<Hotel {self.name}>'
+        return f"<Hotel {self.name}>"
 
     def serialize(self):
         return {
@@ -142,15 +151,16 @@ class Hotel(db.Model):
             "hotel_owner": self.owner.serialize()
         }
 
+
 class Room(db.Model):
     __tablename__ = "room"
-    id = db.Column(db.Integer, primary_key=True) 
+    id = db.Column(db.Integer, primary_key=True)
     pet_type = db.Column(db.String(30), nullable=False, default="any")
     hotel_id = db.Column(db.Integer, db.ForeignKey("hotel.id"), nullable=False)
     bookings = db.relationship("Booking", backref=db.backref("room"))
 
     def __repr__(self):
-        return f'<Room_id {self.id}>'
+        return f"<Room_id {self.id}>"
 
     def serialize(self):
         return {
@@ -159,15 +169,18 @@ class Room(db.Model):
             "hotel_id": self.hotel_id,
             "bookings": [booking.serialize() for booking in self.bookings]
         }
-        
+
+
 class Booking(db.Model):
-    __tablename__ = 'booking'
-    id = db.Column(db.Integer, primary_key=True) 
+    __tablename__ = "booking"
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     pet_id = db.Column(db.Integer, db.ForeignKey("pets.id"), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey("owner.id"), nullable=False)
     hotel_id = db.Column(db.Integer, db.ForeignKey("hotel.id"), nullable=False)
     room_id = db.Column(db.Integer, db.ForeignKey("room.id"), nullable=False)
-    create_date = db.Column(db.DateTime, nullable=False, default=func.now())
+    create_date = db.Column(db.DateTime, nullable=False,
+                            default=datetime.datetime.now)
     entry_date = db.Column(db.DateTime, nullable=False)
     checkout_date = db.Column(db.DateTime, nullable=False)
     price = db.Column(db.Float(precision=2), nullable=False)
@@ -175,7 +188,7 @@ class Booking(db.Model):
     invoice = db.relationship("Invoice", backref=db.backref("booking"))
 
     def __repr__(self):
-        return f'<Booking {self.id}>'
+        return f"<Booking {self.id}>"
 
     def serialize(self):
         return {
@@ -187,13 +200,15 @@ class Booking(db.Model):
             "hotel_id": self.hotel_id,
             "price": self.price,
             "currency": self.currency,
-            "invoice": self.invoice.serialize() # could we remove this?
+            "invoice": self.invoice.serialize()  # could we remove this?
         }
-    
+
+
 class Invoice(db.Model):
     __tablename__ = "invoice"
     id = db.Column(db.Integer, primary_key=True)
-    booking_id = db.Column(db.Integer, db.ForeignKey("booking.id"), nullable=False)
+    booking_id = db.Column(db.Integer, db.ForeignKey(
+        "booking.id"), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     hotel_id = db.Column(db.Integer, db.ForeignKey("hotel.id"), nullable=False)
     amount = db.Column(db.Float(precision=2), nullable=False)
@@ -202,7 +217,7 @@ class Invoice(db.Model):
     payment_ref = db.Column(db.String(), nullable=True)
 
     def __repr__(self):
-        return f'<Invoice_id {self.id}>'
+        return f"<Invoice_id {self.id}>"
 
     def serialize(self):
         return {
@@ -216,11 +231,12 @@ class Invoice(db.Model):
             "hotel_id": self.hotel_id
         }
 
+
 class Countries_zip_codes(db.Model):
-    __tablename__ = 'countries_zip_codes'
+    __tablename__ = "countries_zip_codes"
     country = db.Column(db.String)
     country_iso = db.Column(db.String, primary_key=True)
     zip_regex = db.Column(db.String)
 
     def __repr__(self):
-        return f'<{self.country_iso}: {self.country}>'
+        return f"<{self.country_iso}: {self.country}>"
