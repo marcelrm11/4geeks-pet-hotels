55e00722d9aa518430f428b7fcc249bb743c72d8
Nicolettastr <nicolettastruggia@hotmail.com>
2023-02-23 17:25:48 +0000

    adding home styles and las section, missing footer section

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,17 +3,54 @@ import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import landing_image from "../../img/homeLandingImage.jpeg";
 import HotelCard from "../component/hotelCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faShieldHeart,
+  faCheck,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   const [hotelsInfo, setHotelsInfo] = useState([]);
+  const [values] = useState([
+    {
+      img: faShieldHeart,
+      value: "Commitment to animal welfare",
+      description: "Caring for your pet's well-being is our top priority.",
+    },
+    {
+      img: faCheck,
+      value: "Ease of use and accessibility",
+      description:
+        "Book your pet's fast and easy with our user-friendly interface.",
+    },
+    {
+      img: faHeart,
+      value: "Trust and security",
+      description: "Trust us to lovingly care for your pet as if it were our own",
+    },
+  ]);
 
   useEffect(() => {
     for (let i = 0; i < store.hotels.length && i < 3; i++) {
       setHotelsInfo(<HotelCard hotel={store.hotels[i]} key={i} index={i} />);
     }
   }, []);
+
+  const bs_values = values.map((item, index) => {
+    return (
+      <div className="d_flex_col  mg-1 vl-container" key={index}>
+        <FontAwesomeIcon
+          className="font-l salmon bg-lighter-blue one_pad round_border"
+          icon={item.img}
+        />
+        <h4 className="mg-tb">{item.value}</h4>
+        <p>{item.description}</p>
+      </div>
+    );
+  });
 
   return (
     <div className="text-center mt-5">
@@ -54,7 +91,11 @@ export const Home = () => {
         </div>
       </section>
 
-      <section className="home_value"></section>
+      <section className="home_value">
+        <div className="value_section d_flex_col bg-darkBlue white_letter mg-1 border-style one_pad">
+          {bs_values}
+        </div>
+      </section>
     </div>
   );
 };
